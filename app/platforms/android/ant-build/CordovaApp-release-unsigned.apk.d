a657d6fd50ed7d285a1688fbb85c2760c674f01d
Eneko Nieto <enekonieto@gmail.com>
2014-12-24 14:32:18 +0100

    release build

---
+++ app/platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/CordovaApp.ap_ \
+/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/classes.dex \
+/home/asus/workspace/AforoKontrola/app/platforms/android/CordovaLib/ant-build/classes.jar \
--- app/platforms/android/ant-build/CordovaApp.ap_.d
+++ app/platforms/android/ant-build/CordovaApp.ap_.d
@@ -1,30 +1,14 @@
 /home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/CordovaApp.ap_ \
- : /home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-port-ldpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-port-hdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-hdpi/icon.png \
+ : /home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-hdpi/icon.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-mdpi/icon.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-land-xhdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-land-ldpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-port-xhdpi/screen.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable/icon.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-land-hdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-land-mdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-port-mdpi/screen.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/res/values/strings.xml \
 /home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-ldpi/icon.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/res/xml/config.xml \
 /home/asus/workspace/AforoKontrola/app/platforms/android/res/drawable-xhdpi/icon.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-mdpi/icon.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable/icon.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-land-mdpi/screen.png \
-/home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-port-mdpi/screen.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-ldpi/icon.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /home/asus/workspace/AforoKontrola/app/platforms/android/assets/_where-is-www.txt \
